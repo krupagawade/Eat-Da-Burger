c9a691abf56b7e175d88ce2ea1ffea33f199b883
Krupa <krupagawade@yahoo.com>
2018-06-27 22:44:52 -0500

    Changes for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "127.0.0.1",
-  port: 3307,
-  user: "NWTrainingUser",
-  password: "NWKrupa123",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+  host: 'o677vxfi8ok6exrd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'pltq2rhd4v050ywr',
+  password: 'bac6tsupozf7hbuy',
+  database: 'burgers_db'
+  });
+}
+
+// var connection = mysql.createConnection({
+//   host: "127.0.0.1",
+//   port: 3307,
+//   user: "NWTrainingUser",
+//   password: "NWKrupa123",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
